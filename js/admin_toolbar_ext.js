c64d598fb576895b4f3ca0cce502b18a102b6715
Paul <gpb@yandex.ru>
2019-09-27 08:54:53 +0400

    init

---
+++ js/admin_toolbar_ext.js
@@ -0,0 +1,73 @@
+(function ($, Drupal) {
+
+  function moveTop(ul, value, duration) {
+    ul.animate({top: '+=' + value}, {duration: duration});
+  };
+
+  function moveBottom(ul, value, duration) {
+    ul.animate({top: '-=' + value}, {duration: duration});
+  };
+
+
+  function onWheel(ul, move_value, duration) {
+
+    let placeStart = null;
+
+    return function (event) {
+
+      const windowHeight = $(window).height();
+
+      const bounding = ul.get(0).getBoundingClientRect();
+
+      if (placeStart === null) {
+        placeStart = bounding.top;
+      }
+
+      if (event.originalEvent.wheelDelta > 0) {
+        if (bounding.top <= (placeStart - move_value)) {
+          moveTop(ul, move_value, duration);
+        }
+
+      } else {
+        const hiddenBottom = windowHeight - bounding.bottom;
+        if (hiddenBottom < 0) {
+          moveBottom(ul, move_value, duration);
+        }
+      }
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+
+  Drupal.behaviors.adminToolbarExt = {
+    attach: function (context, settings) {
+
+      var duration = 0;
+
+
+      var move_value = 20;
+
+      $('.menu-item > ul.toolbar-menu', context).each(function (index) {
+
+        const ul = $(this);
+
+        ul.on({
+          wheel: onWheel(ul, move_value, duration)
+        });
+      });
+
+
+      $('ul:not(.toolbar-menu)', context).on({
+        // mousemove: function () {
+        //   $('li.menu-item--expanded').removeClass('hover-intent');
+        // },
+        // hover: function () {
+        //   $('li.menu-item--expanded').removeClass('hover-intent');
+        // }
+      });
+
+    }
+  };
+})(jQuery, Drupal);
+
